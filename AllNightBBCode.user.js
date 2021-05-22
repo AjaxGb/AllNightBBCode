e8f0cf0c17d0868e84be541f02cb8315e4424b0d
Sol Toder <ajaxgb@gmail.com>
2021-05-22 00:55:50 -0400

    Add [quote] support

--- AllNightBBCode.user.js
+++ AllNightBBCode.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name         All Night BBCode
-// @version      0.2
+// @version      0.3
 // @description  Fix broken BBCode on the All Night Laundry comic
 // @author       AjaxGb
 // @match        http*://www.all-night-laundry.com/post/*
 // @icon         http://www.all-night-laundry.com/static/favicon.ico
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    GM_addStyle('.ui-widget-content .quote { border-color: rgb(210, 210, 210); }');
 
     const origMSPAFontSizePx = 13;
 
@@ -20,7 +22,7 @@
         return display;
     }
 
-    function walkNodes(walker, {addToNode = null, addToName = null} = {}) {
+    function walkNodes(walker, {addToNode, addToName}) {
         for (let node = walker.currentNode; node;) {
             let newAddTo = null;
             switch (node.nodeType) {
@@ -29,7 +31,7 @@
                     if (walker.firstChild()) {
                         console.group(getElementDisplayName(node), node);
                         try {
-                            walkNodes(walker);
+                            walkNodes(walker, {});
                         } finally {
                             console.groupEnd();
                         }
@@ -42,48 +44,61 @@
                 case Node.TEXT_NODE:
                 {
                     console.log('TEXT:', node);
-                    const tagMatch = node.data.match(/\[size=(\d+)\]|\[\/size\]/);
+                    const tagMatch = node.data.match(/\[size=(\d+)\]|\[quote=([^;\]]+);\d+\]|\[\/(size|quote)\]/);
                     if (tagMatch) {
-                        const [tagText, sizePxText] = tagMatch;
+                        const [tagText, sizePxText, quoteName, endTag] = tagMatch;
                         const tagTextNode = node.splitText(tagMatch.index);
                         const nextTextNode = tagTextNode.splitText(tagText.length);
                         //  node  tagTextNode nextTextNode
                         // "blah"   "[tag]"   "blah"
 
-                        if (tagText.startsWith('[size=')) {
-                            const sizePx = parseInt(sizePxText);
-                            const span = document.createElement('span');
-                            const currFontSize = tagTextNode.parentElement.computedStyleMap().get('font-size');
-                            if (currFontSize.unit !== 'px') {
-                                throw new Error('computedStyleMap() font-size was not px: '+currFontSize.unit);
-                            }
-                            span.style.fontSize = `${Math.round(currFontSize.value * sizePx / origMSPAFontSizePx)}px`;
-                            tagTextNode.replaceWith(span);
-
-                            walker.currentNode = nextTextNode;
-                            console.group(tagText);
-                            try {
-                                walkNodes(walker, {mspaFontSizePx: sizePx, addToNode: span, addToName: 'size'});
-                            } finally {
-                                console.groupEnd();
-                            }
-                            walker.currentNode = node;
-
-                            const oldNode = node;
-                            node = walker.nextSibling();
-                            if (addToNode) {
-                                addToNode.append(oldNode);
-                            }
-
-                        } else if (tagText === '[/size]') {
-                            if (addToName !== 'size') {
-                                throw new Error('Closed [size] tag when no such tag available');
+                        if (endTag) {
+                            if (addToName !== endTag) {
+                                throw new Error(`Closed [${endTag}] tag when no such tag available`);
                             }
                             tagTextNode.replaceWith(nextTextNode);
                             if (addToNode) {
                                 addToNode.append(node);
                             }
                             return;
+                        } else {
+                            let walkArgs;
+
+                            if (tagText.startsWith('[size=')) {
+                                const sizePx = parseInt(sizePxText);
+                                const span = document.createElement('span');
+                                const currFontSize = tagTextNode.parentElement.computedStyleMap().get('font-size');
+                                if (currFontSize.unit !== 'px') {
+                                    throw new Error('computedStyleMap() font-size was not px: '+currFontSize.unit);
+                                }
+                                span.style.fontSize = `${Math.round(currFontSize.value * sizePx / origMSPAFontSizePx)}px`;
+                                tagTextNode.replaceWith(span);
+
+                                walkArgs = {addToNode: span, addToName: 'size'};
+
+                            } else if (tagText.startsWith('[quote=')) {
+                                const quoteBlock = document.createElement('div');
+                                quoteBlock.className = 'quote';
+                                const quoteTitle = document.createElement('span');
+                                quoteTitle.className = 'quote_author';
+                                const quoteStrong = document.createElement('strong');
+                                quoteStrong.append(quoteName);
+                                quoteTitle.append(quoteStrong);
+                                quoteBlock.append(quoteTitle);
+                                quoteBlock.append(document.createElement('br'));
+                                tagTextNode.replaceWith(quoteBlock);
+
+                                walkArgs = {addToNode: quoteBlock, addToName: 'quote'};
+                            }
+
+                            walker.currentNode = nextTextNode;
+                            console.group(tagText);
+                            try {
+                                walkNodes(walker, walkArgs);
+                            } finally {
+                                console.groupEnd();
+                            }
+                            walker.currentNode = node;
                         }
                     }
                     break;
@@ -99,18 +114,24 @@
             throw new Error(`Unclosed [${addToName}] tag`);
         }
     }
-    window.allNightBBCodeWalkNodes = walkNodes;
 
-    const content = document.getElementById('main_content');
     const whatToWalk = NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT | NodeFilter.SHOW_COMMENT;
-    const walker = document.createTreeWalker(content, whatToWalk);
-    
-    if (walker.firstChild()) {
-        console.groupCollapsed('All Night BBCode debug tree');
-        try {
-            walkNodes(walker);
-        } finally {
-            console.groupEnd();
+    function fixBBCode(element) {
+        const walker = document.createTreeWalker(element, whatToWalk);
+        if (walker.firstChild()) {
+            console.groupCollapsed('All Night BBCode debug tree', element);
+            try {
+                walkNodes(walker, {});
+            } catch (err) {
+                alert('All Night BBCode error! ' + err);
+                throw err;
+            } finally {
+                console.groupEnd();
+            }
         }
     }
+    window.allNightBBCode_fixBBCode = fixBBCode;
+
+    fixBBCode(document.getElementById('main_content'));
+    fixBBCode(document.getElementById('notes'));
 })();
